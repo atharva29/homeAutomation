99ec17007a14ecd5f863bcfa86082ec8032b3b62
atharva29 <atharvaajgaonkar29@gmail.com>
2018-06-20 11:55:46 +0530

    Add files via upload

--- website/basic.go
+++ website/basic.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 	"net/http"
 	"time"
@@ -8,30 +9,37 @@ import (
 
 var upgrader = websocket.Upgrader{}
 
+func simpleHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func tickTick(w http.ResponseWriter, r *http.Request) {
+	conn, _ := upgrader.Upgrade(w, r, nil)
+	go func(conn *websocket.Conn) {
+		ch := time.Tick(5 * time.Second)
+		for range ch {
+			conn.WriteJSON(myStruct{
+				// Lat:    19.0274,
+				// Lng:    72.856689,
+				Lat : 18.9322 ,
+				Lng : 72.8264 ,
+				Colour: 0,
+			})
+		}
+	}(conn)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
-
-
-	http.HandleFunc("/ws1", func(w http.ResponseWriter, r *http.Request) {
-		conn, _ := upgrader.Upgrade(w, r, nil)
-		go func(conn *websocket.Conn) {
-			ch := time.Tick(5 * time.Second)
-
-			for range ch {
-				conn.WriteJSON(myStruct{
-					Lat:  19.0274,
-					Lng: 72.856689,
-				})
-			}
-		}(conn)
-	})
+	router := mux.NewRouter()
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("/home/vjti/website")))
+	router.HandleFunc("/", simpleHandler)
+	router.HandleFunc("/ws1", tickTick)
 
 	http.ListenAndServe(":3000", nil)
 }
 
 type myStruct struct {
-	Lat  float32 `json:""`
-	Lng float32 `json:""`
+	Lat    float32 `json:""`
+	Lng    float32 `json:""`
+	Colour int     `json:""`
 }
